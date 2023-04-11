6315e8690f14aaf120de4a86a094300ed1d0be97
qimu <qimu@192.168.0.101>
2023-04-11 22:43:01 +0800

    feat: first commit

---
+++ rollup.config.js
@@ -0,0 +1,35 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+
+const pkg = require("./package.json");
+console.log(dts, 'dts');
+
+export default [
+  {
+    input: "./src/index.ts",
+    output: [
+      {
+        file: pkg.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: pkg.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      resolve(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+      commonjs(),
+    ],
+  },
+  {
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts.default()],
+  },
+];
